25739e9826db7bf55b5432ee7dd82f439540e672
Danny <dannyconcepcion01@gmail.com>
2020-09-20 20:18:47 -0400

    Agregar archivos de proyecto.

---
+++ programa_5_practica_4/Personaje.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_5_practica_4
+{
+    class Personaje
+    {
+        public static void MoverArriba()
+        {
+            
+            Console.WriteLine("Arriba");
+            Console.ReadLine();
+
+        }
+
+        public static void MoverAbajo()
+
+        {
+            
+            Console.WriteLine("Abajo");
+            Console.ReadLine();
+
+        }
+        public static void MoverIzquierda()
+        {
+           
+            Console.WriteLine("Mover a la izquierda");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ programa_5_practica_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace programa_5_practica_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetCursorPosition(5, 7);
+            Console.WriteLine("Jugando con las posiciones");
+
+            //instanciando las clases
+            Personaje.MoverArriba();
+            Personaje.MoverAbajo();
+            Personaje.MoverIzquierda();
+            Mario.MoverArriba();
+            Mario.MoverAbajo();
+            Mario.MoverIzquierda();
+            Koopa.MoverArriba();
+            Koopa.MoverAbajo();
+            Koopa.MoverIzquierda();
+        }
+    }
+}
